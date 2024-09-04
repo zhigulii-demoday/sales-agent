84fae6a1bf3dd48eb315255097ca47a8ae79b163
root <root@snncwljubv.local>
2024-09-04 17:01:32 +0000

    backup

--- db/models.py
+++ db/models.py
@@ -12,6 +12,7 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 BaseConfig.arbitrary_types_allowed = True
 
+from pgvector.sqlalchemy import Vector
 
 class Base(DeclarativeBase):
     __allow_unmapped__ = True
@@ -40,15 +41,40 @@ class Platform(Base):
     deleted_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=text('NULL'))
 
 
-class Companie(Base):
+class Company(Base):
     __tablename__ = "companies"
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[Optional[str]]
+    name: Mapped[Optional[str]]
     description: Mapped[Optional[str]] = mapped_column(TEXT)
 
     created_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
     updated_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
     deleted_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=text('NULL'))
+
+
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    company_id: Mapped[int] = mapped_column(ForeignKey("public.companies.id"))
+    company: Mapped["Company"] = relationship()
+
+    product_cat: Mapped[Optional[str]]
+    product_name: Mapped[Optional[str]]
+
+    review_dt: Mapped[Optional[datetime.datetime]]
+    review_text: Mapped[Optional[str]]
+    topic: Mapped[Optional[str]]
+    sentiment: Mapped[Optional[str]]
+    marketplace: Mapped[Optional[str]]
+    embedding = mapped_column(Vector(20))
+ 
+
+    created_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
+    updated_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=func.CURRENT_TIMESTAMP())
+    deleted_at: Mapped[Optional[datetime.datetime]] = mapped_column(server_default=text('NULL'))
+
 
 class Contact(Base):
     __tablename__ = "contacts"
--- reload_bd.py
+++ reload_bd.py
@@ -4,15 +4,68 @@ import asyncio
 from db import core
 from db import models
 
+from sqlalchemy import text
+
+from db.core import PGSession
+import pandas as pd
+from tqdm import tqdm
+
+import numpy as np 
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 async def main():
+    # print(os.environ.get('POSTGRES_DB'))
+    # async with PGSession() as session:
+    #     await session.execute(text(f"ALTER DATABASE postgres SET SEARCH_PATH TO public;"))
+    #     await session.execute(text(f"CREATE EXTENSION IF NOT EXISTS vector SCHEMA public;"))
+
     async with core.pg_engine.begin() as s:
         print('adding_new_tables...')
         await s.run_sync(
              models.Base.metadata.create_all
         )
         print('added')
+
+
+    async with PGSession() as session:
+        df = pd.read_excel('Tasty_cofee_data.xlsx')
+        df = df.replace(np.nan, None)
+        df = df.replace('-', None)
+        print(df.columns)
+        company = models.Company(
+            name = "tasty_coffee",
+            description = "Российская компания, занимающаяся преимущественно обжаркой кофейных зёрен и их продажей. На обжарочных предприятиях используются ростеры компаний Probat и Loring, дожигатели, дестоунеры, промышленные кофемолки Ditting и прочее оборудование."
+        )
+        session.add(company)
+
+        for index, row in tqdm(df.iterrows(), total=df.shape[0]):
+
+            new = models.Review(
+
+                company = company,
+
+                product_cat = row['Product Category'],
+                product_name = row['Product Name'],
+
+                review_dt = row['Review Date'],
+                review_text = row['Review Text'],
+                topic = row['Topic'],
+                sentiment = row['Sentiment'],
+                marketplace = row['Marketplace'],
+                embedding = None,
+            )
+            session.add(new)
+        
+
+        await session.commit()
+
+
+
 
 if __name__ == '__main__':
     asyncio.run(main())
